b6b6813b7aa11f73cdf155fc229e5b3ca374ed82
SSaabir <159421259+SSaabir@users.noreply.github.com>
2024-11-30 11:36:12 +0530

    pages and routes

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/about'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/signin" element={<SignIn />}/>
+      <Route path="/signup" element={<SignUp />}/>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
   )
 }
