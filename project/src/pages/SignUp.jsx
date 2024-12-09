d6543a477559e815288a69b937830b22f8b22dc8
SSaabir <159421259+SSaabir@users.noreply.github.com>
2024-12-09 10:35:02 +0530

    signup. got stuck on tutorial hell

--- project/src/pages/SignUp.jsx
+++ project/src/pages/SignUp.jsx
@@ -2,8 +2,27 @@ import { Button, TextInput } from 'flowbite-react'
 import React from 'react'
 import {Link } from 'react-router-dom'
 import { Label } from 'flowbite-react'
+import { json } from 'express';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const HandleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value});
+  };
+  const HandleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-5'>
@@ -16,23 +35,23 @@ export default function SignUp() {
         </div>
         <div className='flex-1'>
           {/* right */}
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={HandleSubmit}>
             <div>
               <Label value='Your Username' />
               <TextInput
-              type='text' placeholder='Username' id='username'/>
+              type='text' placeholder='Username' id='username' onChange={HandleChange}/>
             </div>
             
             <div>
               <Label value='Your Email'/>
               <TextInput
-              type='email' placeholder='Email' id='email'/>
+              type='email' placeholder='Email' id='email' onChange={HandleChange}/>
             </div>
 
               <div>
               <Label value='Your Password'/>
               <TextInput
-              type='password' placeholder='Password' id='password'/>
+              type='password' placeholder='Password' id='password' onChange={HandleChange}/>
             </div>
 
             <Button type='submit'>Sign Up</Button>
